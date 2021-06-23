90ea3c065bdb5389b95eb1ebc33ae7810f19c735
Lance Pidor <kyrios213@gmail.com>
2021-06-23 11:24:40 +0800

    First ajax call

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name ='posts'
+
 urlpatterns = [
-    path('', views.test, name="test"),
+    path('', views.post_list_and_create, name="home"),
+
+    path('hello-world/', views.hello_world_view, name='hello-world'),
+    path('data/', views.load_post_data_view, name='post-data'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 
-def test(request):
-    return HttpResponse("Placeholder")
+from .models import Post
+
+
+def post_list_and_create(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/main.html', {'posts': posts})
+
+def load_post_data_view(request):
+    posts = Post.objects.all()
+    data = []
+    for post in posts:
+        item = {
+            'id': post.id,
+            'title': post.title,
+            'body': post.body,
+            'author': post.author.user.username
+        }
+        data.append(item)
+    return JsonResponse({'data': data})
+
+def hello_world_view(request):
+    return JsonResponse({'text': 'hello world'})
