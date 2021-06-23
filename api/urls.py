fc2577744bd3b4ed83c2dfdebfc82eb05935277b
Lance Pidor <kyrios213@gmail.com>
2021-06-23 21:36:00 +0800

    Like button and cards

--- api/urls.py
+++ api/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', views.post_list_and_create, name="home"),
 
     path('hello-world/', views.hello_world_view, name='hello-world'),
-    path('data/', views.load_post_data_view, name='post-data'),
+    path('data/<int:num_posts>/', views.load_post_data_view, name='post-data'),
 ]
--- api/views.py
+++ api/views.py
@@ -8,18 +8,25 @@ def post_list_and_create(request):
     posts = Post.objects.all()
     return render(request, 'posts/main.html', {'posts': posts})
 
-def load_post_data_view(request):
-    posts = Post.objects.all()
+def load_post_data_view(request, num_posts):
+    visible = 3
+    upper = num_posts
+    lower = upper - visible
+    size = Post.objects.all().count()
+
+    posts = Post.objects.all().order_by('id')
     data = []
     for post in posts:
         item = {
             'id': post.id,
             'title': post.title,
             'body': post.body,
-            'author': post.author.user.username
+            'liked': True if request.user in post.liked.all() else False,
+            'count': post.like_count,
+            'author': post.author.user.username,
         }
         data.append(item)
-    return JsonResponse({'data': data})
+    return JsonResponse({'data': data[lower:upper], 'size': size})
 
 def hello_world_view(request):
     return JsonResponse({'text': 'hello world'})
